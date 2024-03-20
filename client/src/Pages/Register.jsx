a0e93c5361108146d4f7a875726f2d80f3b86445
SRUJANGOWDA01 <srujangowda46@gmail.com>
2024-03-20 12:12:39 +0530

    F.E for server

---
+++ client/src/Pages/Register.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <div className="container">
+      <div className="row">
+         <div className="col-md-6 col-lg-6 col-sm-12">
+          
+         </div>
+         <div className="col-md-6 col-lg-6 col-sm-12">
+
+         </div>
+      </div>
+    </div>
+  )
+}
+
+export default Register
